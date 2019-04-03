035cb85d9b906dc78a8e53629ca139398b959cfe
vuk <info@vuk.bg.it>
2019-04-03 16:19:43 +0200

    composer.json and functions

---
+++ src/functions.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Simplex;
+
+if (! function_exists('Simplex\test')) { 
+    /**
+     * Helper for defining a value.
+     *
+     * @param mixed $value
+     */
+    function test() : null
+    {
+        r('test');
+    }
+}
