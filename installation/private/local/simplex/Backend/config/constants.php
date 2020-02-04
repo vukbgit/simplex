fbb0b50d3eafd2ee9b33b33ceb08fd757c69e7ea
vukbgit <info@vuk.bg.it>
2020-02-04 11:36:31 +0100

    BACKEND_NAVIGATION_PATH constant

--- installation/private/local/simplex/Backend/config/constants.php
+++ installation/private/local/simplex/Backend/config/constants.php
@@ -1,2 +1,2 @@
 <?php
-define('AREA_NAVIGATION_PATH', 'private/local/simplex/Backend/config/navigation.php');
+define('BACKEND_NAVIGATION_PATH', 'private/local/simplex/Backend/config/navigation.php');
