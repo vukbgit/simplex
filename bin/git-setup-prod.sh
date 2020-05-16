8d38a3402f3c7dd4e78dd0fc068738f5e4e67a4d
vukbgit <info@vuk.bg.it>
2020-05-16 18:48:36 +0200

    repository URL as script argument for git-setup-prod.sh

--- bin/git-setup-prod.sh
+++ bin/git-setup-prod.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 #first argument is repository URL, for script automation
 repositoryUrl=$1
-echo "Have you created repository and made first commit?"
-select yn in "Yes" "No"; do
-    case $yn in
-    Yes ) 
-        rm -rf .git
+
+if [ -z "$repositoryUrl" ]
+        then
+                echo "Have you created repository and made first commit?"
+                        select yn in "Yes" "No"; do
+                                case $yn in
+                                Yes ) 
+                                        rm -rf .git
+                                        git init
+                                        read -p "Repository URL:" repositoryUrl
+                                        git remote add origin $repositoryUrl
+                                        git fetch --all
+                                        git reset --hard origin/master
+                                        git branch --set-upstream-to=origin/master
+                                        break;;
+                                No ) exit;;
+                                esac
+                        done
+else
         git init
-	if [ -z "$repositoryUrl" ]
-		then
-		        read -p "Repository URL:" repositoryUrl
-	fi
         git remote add origin $repositoryUrl
         git fetch --all
         git reset --hard origin/master
         git branch --set-upstream-to=origin/master
-        break;;
-    No ) exit;;
-    esac
-done
+fi
+
