847673f41802782978eb48a54fe4d4e13e279ccd
vukbgit <info@vuk.bg.it>
2020-06-12 17:09:15 +0200

    update-all.sh handles both local composer scripts ad defaults to system composer command

--- bin/update-all.sh
+++ bin/update-all.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 private/share/vukbgit/simplex/bin/clean-cache.sh
 git pull
-./composer.sh update
+if [ -f ./composer.sh ]; then
+    ./composer.sh update
+else
+    composer update
+fi
 ./yarn.sh install
