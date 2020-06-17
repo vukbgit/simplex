be81e94e2619572e51d848f089422ed8457879cc
vukbgit <info@vuk.bg.it>
2020-06-17 21:15:02 +0200

    date field into CRUDL config template

--- installation/private/local/simplex/SubjectGroup/Subject/config/crudl.php
+++ installation/private/local/simplex/SubjectGroup/Subject/config/crudl.php
@@ -22,7 +22,23 @@ return (object) [
             ],
             //input filter see https://www.php.net/manual/en/filter.filters.php
             'inputFilter' => ['filter' => FILTER_SANITIZE_STRING, 'flags' => FILTER_FLAG_NO_ENCODE_QUOTES],
-        ]
+        ],
+        'DATE-FIELD' => (object) [
+            'table' => (object)[
+                //boolean defaults to true
+                'filter' => true,
+                //boolean defaults to false
+                'total' => false
+            ],
+            //input filter see https://www.php.net/manual/en/filter.filters.php
+            'inputFilter' => [
+                'filter' => FILTER_VALIDATE_REGEXP,
+                //anglo-saxon date format
+                'options' => array('regexp'=>'/^[0-9]{4}-[0-9]{2}-[0-9]{4}$/')
+                //italian format
+                'options' => array('regexp'=>'/^[0-9]{2}-[0-9]{2}-[0-9]{4}$/')
+            ],
+        ],
     ],
     /*//to build a label when subject is ancestor, fields names or raw strings
     'labelTokens' => [
