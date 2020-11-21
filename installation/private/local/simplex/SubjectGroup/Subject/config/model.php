c94b9ba64b14eb672dfb68062c5c2f12c0664ed9
vukbgit <info@vuk.bg.it>
2020-11-21 12:01:10 +0100

    primaryKeyAlias property in subject model configuration draft

--- installation/private/local/simplex/SubjectGroup/Subject/config/model.php
+++ installation/private/local/simplex/SubjectGroup/Subject/config/model.php
@@ -5,6 +5,9 @@ return (object) [
     'table' => 'TABLE-NAME',
     'view' => 'VIEW-NAME',
     'primaryKey' => 'PRIMARY-KEY-FIELD',
+    //primary key alias is useful in contexts where table primary key proper name cannot be used
+    //i.e: all of schema table have primary key field named 'id', when used as foreign key in db views or into routes definitions these fields *will* be aliased
+    //'primaryKeyAlias' => 'PRIMARY-KEY-FIELD-ALIAS',
     'uploads' => [
         'UPLOAD-KEY' => [
             'OUTPUT-WITHOUT-HANDLER-KEY' => (object) [],
