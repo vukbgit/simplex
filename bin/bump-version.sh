9960f7633e1bf6bdee6a1459118c87657e3e5f22
vukbgit <info@vuk.bg.it>
2022-06-20 10:12:29 +0200

    bump version script

--- bin/bump-version.sh
+++ bin/bump-version.sh
@@ -3,6 +3,6 @@ HIGHLIGHT_COLOR='\033[0;33m'
 ver-bump -p origin
 git checkout development
 last_branch=$(git branch --contains `git rev-list --tags --max-count=1` release*)
-printf "\n\n${HIGHLIGHT_COLOR}merging development branch with "$last_branch
+printf "\n\n${HIGHLIGHT_COLOR}merging development branch with ${last_branch}"
 git merge $last_branch
 git push --set-upstream origin development
