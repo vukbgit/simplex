b7cb070cbae66c55fea8857480b7711289864488
vukbgit <info@vuk.bg.it>
2022-06-20 18:14:10 +0200

    regexp for subject primary key in route draft

--- installation/private/local/simplex/SubjectGroup/Subject/config/routes.draft.php
+++ installation/private/local/simplex/SubjectGroup/Subject/config/routes.draft.php
@@ -31,9 +31,9 @@ $modelConfig = require 'model.php';
 return [
     [
         'method' => ['GET','POST'],
-        'route' => sprintf('/{area:%s}/{subject:%s}/{action}[/{%s}]', $area, $subject, $modelConfig->primaryKey),
+        'route' => sprintf('/{area:%s}/{subject:%s}/{action}[/{%s:\d}]', $area, $subject, $modelConfig->primaryKey),
         //children route
-        //'route' => sprintf('/{area:%s}/{ancestor0:ANCESTOR-SUBJECT}/{ANCESTOR-PRIMARY-KEY}/{subject:%s}/{action}[/{%s}]', $area, $subject, $modelConfig->primaryKey),
+        //'route' => sprintf('/{area:%s}/{ancestor0:ANCESTOR-SUBJECT}/{ANCESTOR-PRIMARY-KEY}/{subject:%s}/{action}[/{%s:\d}]', $area, $subject, $modelConfig->primaryKey),
         'handler' => [
             sprintf('%s-controller', $subject),
             [
