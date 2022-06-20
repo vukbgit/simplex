8300c5cfc1cf119abec07783e32ba0b4a9592f25
vukbgit <info@vuk.bg.it>
2022-06-20 13:25:20 +0200

    bump script with explicit version

--- bin/bump-version.sh
+++ bin/bump-version.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+#first argument is explicit version
+VERSION=$1
 HIGHLIGHT_COLOR='\033[0;33m'
-ver-bump -p origin
+if [ -n "$VERSION" ]
+  ver-bump -v $VERSION -p origin
+else
+  ver-bump -p origin
+fi
 git checkout development
 last_branch=$(git branch --contains `git rev-list --tags --max-count=1` release*)
 printf "\n\n${HIGHLIGHT_COLOR}merging development branch with ${last_branch}\n"
