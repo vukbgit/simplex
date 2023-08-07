cbf2dceb0ab0223bbec66ff949aecd0c2961f2f0
vukbgit <info@vuk.bg.it>
2023-08-07 11:20:22 +0200

    fix: check template filter/function existence before adding

--- src/TwigExtended.php
+++ src/TwigExtended.php
@@ -5,6 +5,7 @@ namespace Simplex;
 
 use Twig\Environment;
 use Twig\TwigFilter;
+use Twig\TwigFunction;
 use Twig\Loader\LoaderInterface;
 use Twig\Extra\Intl\IntlExtension;
 use jblond\TwigTrans\Translation;
@@ -17,44 +18,80 @@ use Aptoma\Twig\Extension\MarkdownEngine;
 */
 class TwigExtended extends Environment
 {
-    /**
-     * Constructor.
-     * @param Twig\Loader\LoaderInterface $loader
-     * @param array $loader: 
-     */
-    public function __construct(LoaderInterface $loader, array $options = [])
-    {
-        parent::__construct($loader, $options);
-        //internationalization
-        //$this->addExtension(new \Twig_Extensions_Extension_I18n());
-        $this->addExtension(new Translation());
-        $filter = new TwigFilter(
-            'trans', 
-            /*function ($context, $string) {
-                return Translation::transGetText($string, $context);
-            },*/
-            'gettext',
-            [
-              //'needs_context' => true,
-              'is_safe_callback' => 'twig_escape_filter_is_safe',
-            ]
-        );
-        $this->addFilter($filter);
-        //Twig IntlExtension
-        $this->addExtension(new IntlExtension());
-        //markdown support
-        $markdownEngine = new MarkdownEngine\MichelfMarkdownEngine();
-        $this->addExtension(new MarkdownExtension($markdownEngine));
-        //set context variable
-        $this->addExtension(new class extends \Twig\Extension\AbstractExtension {
-          public function getFunctions() {
-            return [
-              new \Twig\TwigFunction('setContextVar', [$this, 'setContextVar'], ['needs_context' => true]),
-            ];
-          }
-          public function setContextVar(&$context, $name, $value) {
-            $context[$name] = $value;
-          }
-        });
+  /**
+   * @var array $filters
+   */
+  private $filters = [];
+  
+  /**
+   * @var array $functions
+   */
+  private $functions = [];
+  
+  /**
+    * Constructor.
+    * @param Twig\Loader\LoaderInterface $loader
+    * @param array $loader: 
+    */
+  public function __construct(LoaderInterface $loader, array $options = [])
+  {
+    parent::__construct($loader, $options);
+    //internationalization
+    //$this->addExtension(new \Twig_Extensions_Extension_I18n());
+    $this->addExtension(new Translation());
+    $filter = new TwigFilter(
+        'trans', 
+        /*function ($context, $string) {
+            return Translation::transGetText($string, $context);
+        },*/
+        'gettext',
+        [
+          //'needs_context' => true,
+          'is_safe_callback' => 'twig_escape_filter_is_safe',
+        ]
+    );
+    $this->addFilter($filter);
+    //Twig IntlExtension
+    $this->addExtension(new IntlExtension());
+    //markdown support
+    $markdownEngine = new MarkdownEngine\MichelfMarkdownEngine();
+    $this->addExtension(new MarkdownExtension($markdownEngine));
+    //set context variable
+    $this->addExtension(new class extends \Twig\Extension\AbstractExtension {
+      public function getFunctions() {
+        return [
+          new \Twig\TwigFunction('setContextVar', [$this, 'setContextVar'], ['needs_context' => true]),
+        ];
+      }
+      public function setContextVar(&$context, $name, $value) {
+        $context[$name] = $value;
+      }
+    });
+  }
+  
+  /**
+    * Adds a filter
+    * @param TwigFilter $filter
+    */
+  public function addFilter(TwigFilter $filter)
+  {
+    if(!in_array($filter->getName(), $this->filters)) {
+      $this->filters[] = $filter->getName();
+      parent::addFilter($filter);
     }
+      
+  }
+  
+  /**
+    * Adds a function
+    * @param TwigFunction $function
+    */
+  public function addFunction(TwigFunction $function)
+  {
+    if(!in_array($function->getName(), $this->functions)) {
+      $this->functions[] = $function->getName();
+      parent::addFunction($function);
+    }
+      
+  }
 }
