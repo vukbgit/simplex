bd9656873a0f8e2c355a71a70ecb93ab3c5be6de
vukbgit <info@vuk.bg.it>
2023-11-01 09:36:41 +0100

    stubs for LSP editors

---
+++ stubs/global-constants.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * These constants are dynamically created into bootstrap.php from varibales declared into config.ini
+ * This stub file makes LSP capable editors (like VS Code) aware of these variables
+ */
+define('ENVIRONMENT', '');
+define('ABS_PATH_TO_ROOT', '');
+define('TMP_DIR', '');
+define('PHP_CLI', '');
