bda6a46bb9ca4623003a5ba83a04ab05f87f5477
vukbgit <info@vuk.bg.it>
2024-02-01 12:29:37 +0100

    fix: ERP sidebar toggle labels default

--- refactor/3.14.26.php
+++ refactor/3.14.26.php
@@ -3,4 +3,4 @@
  * Refactor files get included into bin/refactor.php which passes $refactor object
  */
 $refactor->setVerbose(true);
-$refactor->outputMessage('e', 'WARNING: open/close labels have been added to ERP sidebar, CSS has been added to private/share/packagist/vukbgit/simplex/src/Erp/sass/navigation.css which is by default included into private/share/packagist/vukbgit/simplex/src/Erp/sass/ERP.css which is by default included into private/local/simplex/Backend/sass/backend.scss, so it is probably necessary to re-compile this file;');
+$refactor->outputMessage('e', 'WARNING: open/close labels have been added to ERP sidebar, to activate them add to backend.twig (or other ERP area)  sideBarToggleLabels variable set to "true", besides CSS has been added to private/share/packagist/vukbgit/simplex/src/Erp/sass/navigation.css which is by default included into private/share/packagist/vukbgit/simplex/src/Erp/sass/ERP.css which is by default included into private/local/simplex/Backend/sass/backend.scss, so it is probably necessary to re-compile this file;');
