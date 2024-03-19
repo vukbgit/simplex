2f08c674adbb60fc52e950f144f17d37a953512f
vukbgit <info@vuk.bg.it>
2024-03-19 12:42:39 +0100

    ERP navigation

--- installation/private/local/simplex/SubjectGroup/Subject/config/subject.php
+++ installation/private/local/simplex/SubjectGroup/Subject/config/subject.php
@@ -1,28 +1,31 @@
 <?php
 require 'variables.php';
 return (object) [
-    'subjectPermissions' => [sprintf('manage-%s', $subject)],
-    'actions' => [
-        'list' => (object) [
-            'permissions' => [sprintf('manage-%s', $subject)],
-        ],
-        'insert-form' => (object) [
-            'permissions' => [sprintf('manage-%s', $subject)],
-        ],
-        'insert' => (object) [
-            'permissions' => [sprintf('manage-%s', $subject)],
-        ],
-        'update-form' => (object) [
-            'permissions' => [sprintf('manage-%s', $subject)],
-        ],
-        'update' => (object) [
-            'permissions' => [sprintf('manage-%s', $subject)],
-        ],
-        'delete-form' => (object) [
-            'permissions' => [sprintf('manage-%s', $subject)],
-        ],
-        'autocomplete' => (object) [
-            'permissions' => [sprintf('manage-%s', $subject), sprintf('view-%s', $subject)],
-        ],
-    ]
+  'subjectPermissions' => [sprintf('manage-%s', $subject)],
+  'actions' => [
+    'list' => (object) [
+      'permissions' => [sprintf('manage-%s', $subject)],
+    ],
+    'insert-form' => (object) [
+      'permissions' => [sprintf('manage-%s', $subject)],
+    ],
+    'insert' => (object) [
+      'permissions' => [sprintf('manage-%s', $subject)],
+    ],
+    'update-form' => (object) [
+      'permissions' => [sprintf('manage-%s', $subject)],
+    ],
+    'update' => (object) [
+      'permissions' => [sprintf('manage-%s', $subject)],
+    ],
+    'delete-form' => (object) [
+      'permissions' => [sprintf('manage-%s', $subject)],
+    ],
+    'autocomplete' => (object) [
+      'permissions' => [sprintf('manage-%s', $subject), sprintf('view-%s', $subject)],
+    ],
+    'set-side-bar-state' => (object) [
+      'permissions' => [sprintf('manage-%s', $subject), sprintf('view-%s', $subject)],
+    ],    
+  ]
 ];
